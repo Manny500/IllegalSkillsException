9f3f3b2d80e1811b3a8b83fdf2762c1700b1cb6c
James Kim <jamesk7@uci.edu>
2017-10-29 18:16:38 -0400

    deleted useless commented code

--- project2/IllegalSkillsException/src/main/webapp/static/app.js
+++ project2/IllegalSkillsException/src/main/webapp/static/app.js
@@ -27,60 +27,10 @@ function loadNavbar(){
 		if(xhr.readyState == 4 && xhr.status ==200){
 			console.log("navbar loading");
 			document.getElementById('navbar').innerHTML = xhr.responseText;
-//			document.getElementById('tx').addEventListener('click', loadTxView, false);
-//			document.getElementById('dashboard').addEventListener('click', loadDashboardView, false);
 		}
 	}
 	xhr.open("GET", "ajaxNavbar", true);
 	xhr.send();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function loadHomepageNavbar(){
-//	console.log("hello")
-//	var xhr = new XMLHttpRequest();
-//	console.log("xhr loaded")
-//	xhr.onreadystatechange = function(){
-//		if(xhr.readyState == 4 && xhr.status == 200){
-//			navbar = xhr.responseText;
-//			console.log(navbar)
-//			document.getElementById('navbar').innerHTML = navbar;
-//			document.getElementById('boardsView').addEventListener('click', loadBoardView, false);
-//		}
-//	}
-//	
-//	xhr.open("GET", "ajaxNavbar", true);
-//	xhr.send();
-//}
-//
-//function loadBoardView(){
-//	console.log('testing board view');
-//}
 
