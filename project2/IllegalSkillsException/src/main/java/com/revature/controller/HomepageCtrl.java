51603f3d49f4fdf2c176f7b718fbfd7b3afc653d
Manuel Tenorio <tenoriom54@yahoo.com>
2017-10-31 10:15:35 -0400

    nothing

--- project2/IllegalSkillsException/src/main/java/com/revature/controller/HomepageCtrl.java
+++ project2/IllegalSkillsException/src/main/java/com/revature/controller/HomepageCtrl.java
@@ -5,14 +5,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.domain.TV2User;
 
-@RestController
+@Controller
 public class HomepageCtrl {
 
 	@RequestMapping("/")
