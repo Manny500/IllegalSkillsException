eaf1d7d146fff885182a950952b7ee07b894448c
Clement Lin <x4383lin@gmail.com>
2017-11-09 21:45:24 -0500

    beans-test.xml idle

--- project2/IllegalSkillsException/src/test/java/com/revature/test/TestIllegalDao.java
+++ project2/IllegalSkillsException/src/test/java/com/revature/test/TestIllegalDao.java
@@ -19,7 +19,7 @@ import com.revature.domain.Task;
 import com.revature.service.AppService;
 
 //@ContextConfiguration(locations = "classpath:beans-test.xml")
-@ContextConfiguration({ "file:src/main/webapp/WEB-INF/beans-test.xml" })
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/beans.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 //@RunWith(MockitoJUnitRunner.class)
 public class TestIllegalDao {
